0b34f7554e45dd00f36e39e33240284172f8c5a2
gerarmc <gerardomelendezpro@gmail.com>
2023-11-22 00:12:53 -0400

    New features implemented

--- js/game.js
+++ js/game.js
@@ -1,17 +1,30 @@
-const circles = document.querySelectorAll('.circle');
-const startButton = document.getElementById('start-button');
-const scoreElement = document.getElementById('score');
-const livesElement = document.getElementById('lives');
+const circles = document.querySelectorAll(".circle");
+const startButton = document.getElementById("start-button");
+const scoreElement = document.getElementById("score");
+const livesElement = document.getElementById("lives");
+
+let timerInterval;
 
 let score = 0;
 let lives = 3;
 let isGameRunning = false;
 
+let goodSound = new Audio("sound/Laser_00.mp3");
+let badSound = new Audio("sound/alert.ogg");
+
+let level = 1;
+
+let timeLeft = 3000;
+
+let remainingTime = 60;
+
 function resetGame() {
   score = 0;
   lives = 3;
+  remainingTime = 60;
   updateScoreAndLives();
 }
+
 function updateScoreAndLives() {
   scoreElement.textContent = `Score: ${score}`;
   livesElement.textContent = `Lives: ${lives}`;
@@ -23,17 +36,36 @@ function handleCircleClick(event) {
 
   const clickedCircle = event.target;
 
-  if (clickedCircle.classList.contains('pattern-1')) {
+  if (clickedCircle.classList.contains("pattern-1")) {
     score++;
-    activateRandomCircle(); 
+    clickedCircle.classList.remove("pattern-1");
+
+    goodSound.currentTime = 0;
+    goodSound.play();
+
+    if (score % 10 === 0) {
+      level++;
+      timeLeft -= 500;
+      console.log("Time left ===>", timeLeft);
+      if (timeLeft === 1000) {
+        alert("You're super fast!!!");
+      }
+    }
+
+    activateRandomCircle();
   } else {
+    console.log("Clicking empty");
     lives--;
 
+    badSound.currentTime = 0;
+    badSound.play();
+
     if (lives === 0) {
-      alert('Game Over! You lost all your lives.');
+      gameOver();
+      alert(
+        `Game Over! You lost all your lives.  Your final score was ${score}.`
+      );
       resetGame();
-    } else {
-      activateRandomCircle(); 
     }
   }
 
@@ -42,31 +74,33 @@ function handleCircleClick(event) {
 
 // Activate a random circle
 function activateRandomCircle() {
-  const inactiveCircles = Array.from(circles).filter(circle => !circle.classList.contains('pattern-1'));
+  const inactiveCircles = Array.from(circles).filter(
+    (circle) => !circle.classList.contains("pattern-1")
+  );
 
   if (inactiveCircles.length > 0) {
     const randomIndex = Math.floor(Math.random() * inactiveCircles.length);
     const randomCircle = inactiveCircles[randomIndex];
 
-    randomCircle.classList.add('pattern-1');
+    randomCircle.classList.add("pattern-1");
 
     setTimeout(() => {
-      randomCircle.classList.remove('pattern-1');
-    }, 800); // 1 second
+      randomCircle.classList.remove("pattern-1");
+    }, timeLeft); // 1 second (I can modified it for different levels)
 
-    
-    randomCircle.addEventListener('click', handleCircleClick);
+    // randomCircle.addEventListener('click', handleCircleClick);
   } else {
-    alert('Congratulations! You completed the level.');
+    alert(
+      `Congratulations! You completed the level.  Your score for the level was ${score}.`
+    );
     resetGame();
   }
 }
 
 // Timer
-let remainingTime = 60;
 
 function updateTimer() {
-  const timerElement = document.getElementById('timer');
+  const timerElement = document.getElementById("timer");
   timerElement.textContent = `Time: ${remainingTime}s`;
 }
 
@@ -77,11 +111,11 @@ function startGame() {
 
   activateRandomCircle();
 
-  // Disabling the start button during the game
+  // Disabling the start button during the game because if its available it just continue to make the pattern faster. It don't start from the beginning.
   startButton.disabled = true;
 
   // Timer update
-  const timerInterval = setInterval(() => {
+  timerInterval = setInterval(() => {
     remainingTime--;
     updateTimer();
 
@@ -93,7 +127,9 @@ function startGame() {
       if (lives > 0) {
         alert(`Congratulations! You won with a score of ${score}.`);
       } else {
-        alert(`Time's up! You lost all your lives. Your final score is ${score}.`);
+        alert(
+          `Time's up! You lost all your lives. Your final score is ${score}.`
+        );
       }
 
       isGameRunning = false;
@@ -103,6 +139,24 @@ function startGame() {
   }, 1000); // = 1 second
 }
 
-startButton.addEventListener('click', () => {
+function gameOver() {
+  clearInterval(timerInterval);
+  startButton.disabled = false;
+  remainingTime = 60;
+  updateScoreAndLives();
+  updateTimer();
+}
+
+startButton.addEventListener("click", () => {
   startGame();
 });
+
+let allCircles = document.getElementsByClassName("circle");
+
+console.log("Line 129 all circles ===>", allCircles);
+
+for (let i = 0; i < allCircles.length; i++) {
+  allCircles[i].addEventListener("click", handleCircleClick);
+}
+
+// [...allCircles].forEach((circle) =>)
